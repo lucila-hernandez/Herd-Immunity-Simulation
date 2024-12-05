92d8608a17a068dc0736305f12d3edc73f1a3905
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2024-12-04 17:09:40 -0800

    updated virus.py

--- virus.py
+++ virus.py
@@ -1,22 +1,20 @@
 class Virus(object):
-    # Properties and attributes of the virus used in Simulation.
+    """Represents a virus with properties and attributes used in the simulation"""
     def __init__(self, name, repro_rate, mortality_rate):
-        # Define the attributes of your your virus
-        # TODO Define the other attributes of Virus
         self.name = name
         self.repro_rate = repro_rate
         self.mortality_rate = mortality_rate
 
 
 
-# Test this class
+# Tested the class
 if __name__ == "__main__":
-    # Test your virus class by making an instance and confirming 
-    # it has the attributes you defined
+    # Created instances of the Virus class
     ebola_virus = Virus("Ebola", 1.9, 0.6)
     chickenpox_virus = Virus("Chickenpox", 0.8, 0.1)
     hiv_virus = Virus("HIV", 0.8, 0.3)
 
+    # Tested attributes of the created viruses
     assert hiv_virus.name == "HIV"
     assert hiv_virus.repro_rate == 0.8
     assert hiv_virus.mortality_rate == 0.3
@@ -29,6 +27,7 @@ if __name__ == "__main__":
     assert ebola_virus.repro_rate == 1.9
     assert ebola_virus.mortality_rate == 0.6
 
+    # Printed details of each virus
     print(f"Ebola Virus: {ebola_virus.name}")
     print(f"Reproduction Rate: {ebola_virus.repro_rate}")
     print(f"Mortality Rate: {ebola_virus.mortality_rate}")
